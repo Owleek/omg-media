7bac8f8e7db36a9c627956f87e13f8a20de9fbba
Dima <windimax@gmail.com>
2019-07-27 10:43:00 +0300

    Скрипты для печатющегося текста

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,20 +1,27 @@
 //= ../vendors/OwlCarousel2-2.3.4/owl.carousel.min.js
+//= ../vendors/typed-v2.0.9/typed.min.js
 //= ../vendors/jquery.inview.min.js
 
 $(document).ready(function(){
+
+  initTypewriter();
+  initTeamParalax();
   casesCarousel();
-  presentTeam();
 
   $('.consult-request__button')
     .click(function() {
-      $('.consult-request').addClass('activated');
+      $('.consult-request')
+        .addClass('activated');
     });
 
   $('.block-fadein')
     .one('inview', function() {
-      $(this).addClass('inview');
+      $(this)
+        .addClass('inview');
     });
+
 });
+
 
 function casesCarousel() {
   const $casesSection = $(".js-cases-section");
@@ -45,7 +52,7 @@ function casesCarousel() {
   }
 }
 
-function presentTeam() {
+function initTeamParalax() {
   var $teamSlideImage = $('.team-slider .team-slider__image'),
       randomImageIndex = random(0, $teamSlideImage.find('img').length - 1);
 
@@ -55,9 +62,80 @@ function presentTeam() {
   }, 500);
 }
 
+function initTypewriter() {
+  $('[data-typed]')
+    .each(function() {
+      $(this)
+        .find('[data-typed_item]')
+        .each(function() {
+          $(this)
+            .css({
+              visibility: 'hidden'
+            })
+            .data('text', $(this).text())
+            .empty();
+
+          $('<span>')
+            .css({
+              height: 0,
+              display: 'inline-block'
+            })
+            .addClass('typedNode')
+            .appendTo($(this))
+      });
+
+    $(this)
+      .data('startTyper', () => {
+        startTyper($(this).find('[data-typed_item]'));
+      });
+  });
+
+  function startTyper($items) {
+    for (var i = 0; i < $items.length; i++) {
+      let $item = $($items[i]);
+      if (!$item.data('type_end')) {
+        $item.css({
+          visibility: 'visible'
+        })
+        let string = $item.data('text'),
+            node = $item.find('.typedNode').get(0);
+        return runTypedPlugin(node, string, () => {
+          $item
+            .text(string)
+            .data('type_end', true);
+            startTyper($items);
+        });
+      };
+    }  
+  };
+
+  function runTypedPlugin(node, string, callback) {
+    new Typed(node, {
+      strings: [string],
+      typeSpeed: 50,
+      backSpeed: 0,
+      fadeOut: true,
+      loop: false,
+      onStringTyped: (pos, self) => {
+        self.destroy();
+      },
+      onDestroy: callback
+    });    
+  };
+
+  setTimeout(() => {
+    $('[data-typed]')
+      .one('inview', function() {
+        $(this).data('startTyper')();
+      });
+  })
+}
+
 function random(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   rand = Math.floor(rand);
   return rand;
 }
+
+
 
