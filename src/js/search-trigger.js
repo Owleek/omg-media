8fc8f9cb0638ab39e102addefe6153157982ed5d
VLadimir <vladimir.h@ati.su>
2019-07-31 01:38:53 +0300

    searchTrigger function completed

--- src/js/search-trigger.js
+++ src/js/search-trigger.js
@@ -4,24 +4,49 @@ function searchTrigger() {
     const $header = $(".header");
     const $searchBar = $header.find(".search-bar");
     const $searchInput = $searchBar.find(".search-bar__input");
-    const $doc = $(document);
     const $clear = $searchBar.find(".search-bar__clear");
+    const $doc = $(document);
 
     $header.addClass("header_search-active");
     $searchInput.focus();
 
+    $doc.on("keyup", onSearchBarEscapeListener);
     $doc.on("click", onSearchBarCloseListener);
     $clear.on("click", function() {
       $searchInput.val("");
+      triggerClearButton();
     });
+    $searchInput.on("keyup", triggerClearButton);
+
+    function triggerClearButton() {
+      if ($searchInput.val()) {
+        $clear.addClass("search-bar__clear_active");
+      } else {
+        $clear.removeClass("search-bar__clear_active");
+      }
+    }
 
     function onSearchBarCloseListener(event) {
       if(!$searchBar.get(0).contains(event.target)) {
-        $header.removeClass("header_search-active");
-        $doc.off("click", onSearchBarCloseListener);
-        $searchInput.val("");
-        $clear.off("click");
+        handleSearchBarClose();
       }
+    }
+
+    function onSearchBarEscapeListener(event) {
+      // 27 escape key code
+      if (event.which === 27) {
+        handleSearchBarClose();
+      }
+    }
+
+    function handleSearchBarClose() {
+      $header.removeClass("header_search-active");
+      $doc.off("click", onSearchBarCloseListener);
+      $doc.off("keyup", onSearchBarEscapeListener);
+      $searchInput.val("");
+      $searchInput.off("keyup", triggerClearButton);
+      triggerClearButton();
+      $clear.off("click");
     }
   });
 }
