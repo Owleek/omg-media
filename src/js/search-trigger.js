fbca98b03777a2fd41de936a041ae740df0b8800
VLadimir <vladimir.h@ati.su>
2019-07-31 01:59:35 +0300

    searchTrigger optimization fix

--- src/js/search-trigger.js
+++ src/js/search-trigger.js
@@ -1,11 +1,13 @@
 function searchTrigger() {
-  $(".js-search-icon").on("click", function(event) {
+  const $header = $(".header");
+  const $searchIcon = $header.find(".js-search-icon");
+  const $searchBar = $header.find(".search-bar");
+  const $searchInput = $searchBar.find(".search-bar__input");
+  const $clear = $searchBar.find(".search-bar__clear");
+  const $doc = $(document);
+
+  $searchIcon.on("click", function(event) {
     event.stopPropagation();
-    const $header = $(".header");
-    const $searchBar = $header.find(".search-bar");
-    const $searchInput = $searchBar.find(".search-bar__input");
-    const $clear = $searchBar.find(".search-bar__clear");
-    const $doc = $(document);
 
     $header.addClass("header_search-active");
     $searchInput.focus();
@@ -17,36 +19,36 @@ function searchTrigger() {
       triggerClearButton();
     });
     $searchInput.on("keyup", triggerClearButton);
-
-    function triggerClearButton() {
-      if ($searchInput.val()) {
-        $clear.addClass("search-bar__clear_active");
-      } else {
-        $clear.removeClass("search-bar__clear_active");
-      }
-    }
-
-    function onSearchBarCloseListener(event) {
-      if(!$searchBar.get(0).contains(event.target)) {
-        handleSearchBarClose();
-      }
-    }
-
-    function onSearchBarEscapeListener(event) {
-      // 27 escape key code
-      if (event.which === 27) {
-        handleSearchBarClose();
-      }
-    }
-
-    function handleSearchBarClose() {
-      $header.removeClass("header_search-active");
-      $doc.off("click", onSearchBarCloseListener);
-      $doc.off("keyup", onSearchBarEscapeListener);
-      $searchInput.val("");
-      $searchInput.off("keyup", triggerClearButton);
-      triggerClearButton();
-      $clear.off("click");
-    }
   });
+
+  function triggerClearButton() {
+    if ($searchInput.val()) {
+      $clear.addClass("search-bar__clear_active");
+    } else {
+      $clear.removeClass("search-bar__clear_active");
+    }
+  }
+
+  function onSearchBarCloseListener(event) {
+    if(!$searchBar.get(0).contains(event.target)) {
+      handleSearchBarClose();
+    }
+  }
+
+  function onSearchBarEscapeListener(event) {
+    // 27 escape key code
+    if (event.which === 27) {
+      handleSearchBarClose();
+    }
+  }
+
+  function handleSearchBarClose() {
+    $header.removeClass("header_search-active");
+    $doc.off("click", onSearchBarCloseListener);
+    $doc.off("keyup", onSearchBarEscapeListener);
+    $searchInput.val("");
+    $searchInput.off("keyup", triggerClearButton);
+    triggerClearButton();
+    $clear.off("click");
+  }
 }
